c76cf9a442d7aef947126859ff2bb7c40078f4b1
RachelKolk <rach.m.obrien@gmail.com>
2019-01-29 15:27:14 -0500

    store created and provider imported

---
+++ todo/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My To Do List</h1>
+        <p>Made Possible by Redux</p>
+      </div>
+    );
+  }
+}
+
+export default App;
